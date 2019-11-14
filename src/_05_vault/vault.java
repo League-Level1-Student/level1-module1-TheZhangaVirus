48603b5819b119cc5c4c84bbe495ff642c57c119
League Student <league@iMac-13.attlocal.net>
2019-11-13 16:58:15 -0800

    11/13/2019
    :smile:

--- src/_05_vault/vault.java
+++ src/_05_vault/vault.java
@@ -1,5 +1,33 @@
 package _05_vault;
+import javax.swing.JOptionPane;
+import java.util.Random;
+
 
 public class vault {
+	
+}
 
+class jamesBond {
+	
+	Random random = new Random();
+	int code = random.nextInt(1000000);
+			
+	public void setCode() {
+
+		int code = random.nextInt(1000000);
+		
+	}
+	
+	public void tryCode() {
+		
+		for(int i = 0; i < 1000000; i++) {
+			if(i == code) {
+				System.out.println(i);
+			} else if(i > code) {
+				break;
+			}
+		}
+		
+	}
+	
 }
