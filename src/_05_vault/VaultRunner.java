098935ca6436be24a99ae789f680ab7a7b3f6af4
League Student <league@iMac-24.attlocal.net>
2019-11-22 16:57:49 -0800

    11/22/2019
    :smile:

---
+++ src/_05_vault/VaultRunner.java
@@ -0,0 +1,16 @@
+package _05_vault;
+
+import _05_vault.vault;
+
+public class VaultRunner {
+
+	public static void main(String[] args) {
+		
+		vault vault = new vault();
+		jamesBond bond = new jamesBond();
+		bond.setCode();
+		bond.tryCode();
+		
+	}
+	
+}
---
+++ src/_06_duck/Duck.java
@@ -0,0 +1,29 @@
+package _06_duck;
+
+
+
+public class Duck {
+
+	private String favoriteFood;
+	private int numberOfFriends;
+
+	Duck(String favoriteFood, int numberOfFriends) {
+	       this.favoriteFood = favoriteFood;
+	       this.numberOfFriends = numberOfFriends;
+	}
+		
+	}
+	
+class Animal {
+	
+	private String animalFavFood;
+	private int animalFriends;
+
+	Animal(String animalFavFood, int animalFriends) {
+		this.animalFavFood = animalFavFood;
+		this.animalFriends = animalFriends;
+		
+	}
+	
+}
+
