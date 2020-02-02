6292a84ef31b25abc6a2e4b428cade19cff97feb
League Student <league@iMac-24.attlocal.net>
2020-02-02 12:58:45 -0800

    02/02/2020

---
+++ src/_07_binary_converter/binary_converter.java
@@ -0,0 +1,18 @@
+package _07_binary_converter;
+
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+
+public class binary_converter {
+
+	public static void main(String[] args) {
+		
+		JPanel panel = new JPanel();
+		JFrame frame = new JFrame();
+		JTextField tfield = new JTextField();
+				
+	}
+	
+}
